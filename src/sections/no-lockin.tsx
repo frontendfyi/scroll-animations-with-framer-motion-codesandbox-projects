61273405bd4376c5b267b0c10e775d93f60bcdb0
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-02-25 14:59:55 +0100

    Added links to frontend fyi and codesandbox.

--- src/sections/no-lockin.tsx
+++ src/sections/no-lockin.tsx
@@ -9,5 +9,25 @@ export const NoLockin = () => (
         you love.
       </span>
     </h2>
+
+    <p className="mx-auto mt-20 max-w-[72rem] text-center text-xl text-black [&_a]:underline">
+      I hope you learned something by watching this rebuild, the code and the
+      video. Check out{" "}
+      <a
+        href="https://frontend.fyi/"
+        target="_blank"
+        rel="noopener nofollow noreferrer"
+      >
+        Frontend.FYI
+      </a>{" "}
+      for more videos, and definitely head over to{" "}
+      <a
+        href="https://projects.codesandbox.io"
+        rel="noopener nofollow noreferrer"
+      >
+        CodeSandbox' website
+      </a>{" "}
+      as well!
+    </p>
   </section>
 );
